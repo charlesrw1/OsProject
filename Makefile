664992a97214566fb264c7e3a3f9a3c58b7269f1
Charlie Weiler <charlierweiler@gmail.com>
2023-06-26 02:18:59 -0700

    kernel startup

--- Makefile
+++ Makefile
@@ -4,19 +4,28 @@ CFLAGS=
 
 KERNELSRC = $(wildcard ./source/*.c)
 KERNELOBJ = $(KERNELSRC:.c=.o)
+KERNELSTART = kernel_start.o
 
 BOOTSRC = source/boot.S
+
+all: osimg
 
 %.o: %.c
 	 $(CC) -o $@ -c $< $(CFLAGS)
 
-kernel: $(KERNELOBJ)
-	$(LD) -o ./bin/kernel.bin $^ -T source/link.ld
+kernel_start.o: source/kernel_start.S
+	nasm -f elf -o kernel_start.o source/kernel_start.S 
+
+# kernal_start.o included in linker script
+kernel: $(KERNELOBJ) $(KERNELSTART)
+	$(LD) -o ./bin/kernel.bin $(KERNELOBJ) -T source/link.ld
+
 
 bootloader:
 	nasm -f bin -o ./bin/boot.bin $(BOOTSRC)
 
 osimg: bootloader kernel
 	cat ./bin/boot.bin ./bin/kernel.bin > ./bin/osimage.bin
+
 clean:
-	rm ./*.o
+	rm -f $(KERNELOBJ)
--- source/kernel.c
+++ source/kernel.c
@@ -1,9 +1,52 @@
 
-void _start()
+typedef struct
 {
-	char* video_memory = (char*) 0xb8000;
-    *video_memory = 'C';
-	for(;;) {}
+	int a;
+	int b;
 
-	int a=0;
+	char buffer[64];
+} MyStructure;
+
+char* welcome_str = "Hello there...";
+static MyStructure astruct;
+static MyStructure astruct2;
+
+void print_hex(char* video_memory, int number);
+void print_hex(char* video_memory, int number)
+{
+	char characters[10];
+	int i;
+	for(i=0;i<10 && number!=0;i++)
+	{
+		int a = number&0xf;
+		if(a<=9) {
+			characters[i]=a+'0';
+		}
+		else {
+			characters[i]=a-10+'A';
+		}
+		number>>=4;
+	}
+	for(int j=i-1;j>=0;j--)
+	{
+		*video_memory=characters[j];
+		video_memory+=2;
+	}
+}
+
+typedef unsigned char bool;
+#define true 1
+#define false 0
+#define NULL 0
+
+void kmain()
+{
+	char* string = "Hello from C!!";
+	char* video_memory = (char*) 0xb8000;
+
+	int number = *(char*)(0x2000+1 + 0x10000);
+	
+	print_hex(video_memory,&welcome_str);
+
+	for(;;){}
 }
